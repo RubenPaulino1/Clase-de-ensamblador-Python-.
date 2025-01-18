6bfb2bf0352f9ffb86f2396ea042988b0ccfe08e
Ruben Alberto Paulino Camacho <122657533+RubenPaulino1@users.noreply.github.com>
2025-01-18 15:38:40 -0400

    Add files via upload

---
+++ Tarea 2.py
@@ -0,0 +1,7 @@
+print ("Programa para capturar nombre y apellido en consola")
+
+nombre = input("Por favor, ingresa tu nombre: ")
+
+apellido = input("Por favor, ingresa tu apellido: ")
+
+print(f"Hola, {nombre} {apellido}!")
